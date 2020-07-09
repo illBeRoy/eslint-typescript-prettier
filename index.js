7ce05e54827f2fa143973ff666d4bae76dda84e3
Roy Sommer <roy@sommer.co.il>
2020-07-09 18:05:35 +0300

    update

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
     'eslint-config-prettier',
     'prettier/@typescript-eslint',
   ],
